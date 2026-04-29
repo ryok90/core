e61734ff5d2dad3963d44f9696daeb0189fac2aa
Wu Zhong <zhongwuzw@qq.com>
2026-04-30 06:35:15 +0800

    (metro-core): add manifest SHA-256 hashes and optional bundle cache layer integration (#4576)
    Co-authored-by: zhongwu <zhongwu.zw@alibaba-inc.com>

--- packages/sdk/src/types/stats.ts
+++ packages/sdk/src/types/stats.ts
@@ -30,6 +30,7 @@ export interface ResourceInfo {
 export interface StatsBuildInfo {
   buildVersion: string;
   buildName: string;
+  hash?: string;
 
   // only appear when enable treeshaking
   target?: string[];
@@ -127,6 +128,7 @@ export interface StatsExpose {
   file: string;
   requires: string[];
   assets: StatsAssets;
+  hash?: string;
 }
 
 export interface Stats<T = BasicStatsMetaData, K = StatsRemoteVal> {
